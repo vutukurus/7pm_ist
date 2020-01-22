3b2a2cff5e7e09792562ad64371dd6af077d7e87
lenovo <vutukurus@gmail.com>
2020-01-22 07:37:24 +0530

    t

--- system.py
+++ system.py
@@ -10,8 +10,17 @@ print (a)
 
 a = subprocess.check_output("dir", shell = True)
 print ("="*10)
+print (a) #string
+
+
+a = subprocess.check_call("dir", shell = True)
+print ("="*10)
 print (a)
 
+
+p = subprocess.Popen(["dir"], stdout=subprocess.PIPE, shell = True)
+a = p.communicate()
+print (a) #list..
 
 
 
